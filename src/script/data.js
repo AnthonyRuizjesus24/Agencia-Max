9161fac053fc6c7c31cfd3f284063b12ed425acf
Max Ruiz <maxruizjesus40@gmail.com>
2024-08-13 13:18:04 -0500

    agregue paginas,componentes,layout y estilos css

---
+++ src/script/data.js
@@ -0,0 +1,32 @@
+export const destinations = [
+    {
+      id: 1,
+      image: '/assets/images/destination1.jpg',
+      title: 'Destino 1',
+      description: 'Descripción del destino 1'
+    },
+    {
+      id: 2,
+      image: '/assets/images/destination2.jpg',
+      title: 'Destino 2',
+      description: 'Descripción del destino 2'
+    }
+    // Más destinos...
+  ];
+  
+  export const offers = [
+    {
+      id: 1,
+      image: '/assets/images/offer1.jpg',
+      title: 'Oferta 1',
+      price: '$500'
+    },
+    {
+      id: 2,
+      image: '/assets/images/offer2.jpg',
+      title: 'Oferta 2',
+      price: '$750'
+    }
+    // Más ofertas...
+  ];
+  
